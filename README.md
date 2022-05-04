d3738f3b5082e5dc1eadac44cb22be1134dfc956
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-05-03 21:34:59 -0500

    get post put delete routes implemented for Categories.. ready for test with Insomnia

--- README.md
+++ README.md
@@ -22,46 +22,58 @@
 
 <h2 id="project-desc">Description</h2>
 
+The etailAPI is an ecommerce api that makes use of object relational model techniques. Object oriented programming abstraction is leveraged alongside SQL database queries using Sequelize.  
+
 <p style='text-align: right;'><a href="#project-title">Back to Top</a></p>
 
 <h2 id="project-live">See it in action!</h2>
 
-[See it live](https://aqueous-forest-83707.herokuapp.com/)
+[See the application in action here]/)
 
 <h2 id="project-captures">Screen captures</h2>
 
 | Image | Description |
 | --- | ----------- |
-|  ![screen capture](./public/assets/images/screencapture.gif) | Application in use |
+|  ![]() | Application in use |
 
 
 <h2 id="project-tech">Technology</h2>
 
 - Node.js
 - Express.js
+- Sequelize
 
 <p style='text-align: right;'><a href="#project-title">Back to Top</a></p>
 
 <h2 id="project-inst">Installation</h2>
 
-To install Inquirer.js, run the following command in your local directory
+To install required packages, run the following command in your local directory upon forking the repository.
 
     `npm i`
 
 <p style='text-align: right;'><a href="#project-title">Back to Top</a></p>
 
 <h2 id="project-usage">Usage</h2>
-Recommended prerequisites include node.js version 16.2.0 or later. To execute the program run the following command in your local directory
+Recommended prerequisites include node.js version 16.2.0 or later. 
+
+To populate your database with relevant data for manipulation.
+
+    `npm run seed`
+
+To execute the program run the following command in your local directory
 
     `node server.js`
 
 <p style='text-align: right;'><a href="#project-title">Back to Top</a></p>
 
-<!-- <h2 id="project-ref">References</h2> -->
+<h2 id="project-ref">References/Resources</h2> 
+
+- [Insomnia](https://insomnia.rest/download)
+- [MySQL Workbench](https://www.mysql.com/products/workbench/)
+- [Sequelize](https://sequelize.org/docs/v6/category/core-concepts/)
 
 
-
-<!-- <p style='text-align: right;'><a href="#project-title">Back to Top</a></p> -->
+<p style='text-align: right;'><a href="#project-title">Back to Top</a></p>
 
 <h2 id="project-contact">Questions / Contact</h2>
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,27 +2,89 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    const categories = await Category.findAll({
+      // be sure to include its associated Products
+      // Add products as a model to JOIN with
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+// GET a single category
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      // Add products as a model to JOIN with
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category could be found by that id...' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+// POST / create a single category
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!category[0]) {
+      res.status(404).json({ message: 'No category could be found by that id...' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+
+// DELETE a single category
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category could be found by that id...' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
